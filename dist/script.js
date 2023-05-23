f11b8ba652744c803d763006887e25ee10625522
adethnunez <bernadethnunez005@gmail.com>
2023-05-23 15:06:50 +0800

    up

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,11 @@
+const faqs = document.querySelectorAll(".faq__question h4");
+faqs.forEach((baba) => {
+  baba.addEventListener("click", () => {
+    baba.nextElementSibling.classList.toggle("open");
+    baba.querySelector("i").classList.toggle("open");
+  });
+});
+
 const toggleMenu = document.querySelector(".toggle__menu");
 const mobileNav = document.querySelector(".header__actions");
 toggleMenu.addEventListener("click", () => {
@@ -5,10 +13,11 @@ toggleMenu.addEventListener("click", () => {
   mobileNav.classList.toggle("open");
 });
 
-const links = document.querySelectorAll(".links__drop__item h4");
+const links = document.querySelectorAll(".footer__items h4");
 links.forEach((baba) => {
   baba.addEventListener("click", () => {
     baba.nextElementSibling.classList.toggle("open");
     baba.querySelector("i").classList.toggle("open");
   });
 });
+
